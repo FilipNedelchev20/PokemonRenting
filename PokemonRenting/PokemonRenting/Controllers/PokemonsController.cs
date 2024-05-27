ee99f5ad196db91d33e6de381949ceef904df152
FIlipNedelchev20 <filnedelchev@gmail.com>
2024-05-27 08:49:44 +0300

    Creating the App Context

---
+++ PokemonRenting/PokemonRenting/Controllers/PokemonsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PokemonRenting.Web.Controllers
+{
+    public class PokemonsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+
+}
