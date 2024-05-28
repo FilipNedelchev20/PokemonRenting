7916196b69d3ef3d2f0da95ad2724a3375fe8ca4
FIlipNedelchev20 <filnedelchev@gmail.com>
2024-05-28 08:53:53 +0300

    Creating a profile

---
+++ PokemonRenting/PokemonRenting/Mapper/PokemonProfile.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using PokemonRenting.Web.Models.ViewModels.Pokemon;
+using PokemonRentingModels;
+
+namespace PokemonRenting.Web.Mapper
+{
+    public class PokemonProfile:Profile
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public PokemonProfile(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+
+
+
+            CreateMap<Pokemon, PokemonViewModel>();
+            CreateMap<CreatePokemonViewModel, Pokemon>()
+                .ForMember(destination => destination.PokemonImage, 
+                opt=> opt.MapFrom(source=> SaveImageFile(source.PokemonImageUrl)));
+        }
+
+        public PokemonProfile()
+        {
+            
+        }
+
+        private string SaveImageFile(IFormFile pokemonImageUrl)
+        {
+            if (pokemonImageUrl == null || pokemonImageUrl.Length == 0)
+                return null;
+
+            var fileName = Guid.NewGuid().ToString() + Path.GetExtension();
+            var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, fileName);
+            using (var fileStream = new FileStream(imagePath, FileMode.Create))
+            {
+                File.CopyTo(fileStream);
+            }
+        }
+    }
+}
