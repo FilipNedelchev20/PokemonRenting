d2606e4e6418a2f7e9cc3d9c9876776a493165f2
FIlipNedelchev20 <filnedelchev@gmail.com>
2024-05-28 08:52:51 +0300

    ExceptionHandlerMiddleware

---
+++ PokemonRenting/PokemonRenting/CustomMiddleWare/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Net;
+
+namespace PokemonRenting.Web.CustomMiddleWare
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, ILogger<ExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "An unhandled exception occurred.");
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync("{\"message\": \"An error occurred. Please try again later.}");
+            }
+        }
+    }
+}
