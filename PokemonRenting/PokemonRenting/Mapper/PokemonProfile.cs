98042fc997c2f1d5bfff3eb9d65cabe8abb7d653
FIlipNedelchev20 <filnedelchev@gmail.com>
2024-06-03 08:37:09 +0300

    Working on the mapper

--- PokemonRenting/PokemonRenting/Mapper/PokemonProfile.cs
+++ PokemonRenting/PokemonRenting/Mapper/PokemonProfile.cs
@@ -1,40 +1,23 @@
 ﻿using AutoMapper;
 using PokemonRenting.Web.Models.ViewModels.Pokemon;
+using PokemonRenting.Web.Utility;
 using PokemonRentingModels;
 
 namespace PokemonRenting.Web.Mapper
 {
     public class PokemonProfile:Profile
     {
-        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IWebHostEnvironment _WebHostEnvironment;
         public PokemonProfile(IWebHostEnvironment webHostEnvironment)
         {
-            _webHostEnvironment = webHostEnvironment;
 
-
-
+            _WebHostEnvironment = webHostEnvironment;
             CreateMap<Pokemon, PokemonViewModel>();
             CreateMap<CreatePokemonViewModel, Pokemon>()
                 .ForMember(destination => destination.PokemonImage, 
-                opt=> opt.MapFrom(source=> SaveImageFile(source.PokemonImageUrl)));
+                opt=> opt.MapFrom(source=> new ImageUpload(_WebHostEnvironment).SaveImageFile(source.PokemonImageUrl)));
         }
 
-        public PokemonProfile()
-        {
-            
-        }
-
-        private string SaveImageFile(IFormFile pokemonImageUrl)
-        {
-            if (pokemonImageUrl == null || pokemonImageUrl.Length == 0)
-                return null;
-
-            var fileName = Guid.NewGuid().ToString() + Path.GetExtension();
-            var imagePath = Path.Combine(_webHostEnvironment.WebRootPath, fileName);
-            using (var fileStream = new FileStream(imagePath, FileMode.Create))
-            {
-                File.CopyTo(fileStream);
-            }
-        }
+       
     }
 }
