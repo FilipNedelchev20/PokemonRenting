c169173ccc6c989c163b999cb60155a99235f84b
FIlipNedelchev20 <filnedelchev@gmail.com>
2024-06-03 08:38:30 +0300

    The image upload class

---
+++ PokemonRenting/PokemonRenting/Utility/ImageUpload.cs
@@ -0,0 +1,31 @@
+﻿namespace PokemonRenting.Web.Utility
+{
+    public class ImageUpload
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ImageUpload(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public string SaveImageFile(IFormFile pokemonImageUrl)
+        {
+            string webRootPath = _webHostEnvironment.WebRootPath;
+            string uploadPath = Path.Combine(webRootPath, "upload");
+            if (!Directory.Exists(uploadPath))
+            {
+                Directory.CreateDirectory(uploadPath);
+            }
+
+
+            string fileName = Guid.NewGuid().ToString() +
+                Path.GetExtension(pokemonImageUrl.FileName);
+            string filePath = Path.Combine(uploadPath, fileName);
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                pokemonImageUrl.CopyTo(fileStream);
+            }
+            return Path.Combine("upload", fileName);
+        }
+    }
+}
