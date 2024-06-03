e17af6007046e8d3439782a494d90fd518f1f325
FIlipNedelchev20 <filnedelchev@gmail.com>
2024-06-03 08:39:53 +0300

    Adding features to the program.cs

--- PokemonRenting/PokemonRenting/Program.cs
+++ PokemonRenting/PokemonRenting/Program.cs
@@ -1,50 +1,110 @@
+﻿
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
+using PokemonRenting.Models;
 using PokemonRenting.Repositories;
+using PokemonRenting.Repositories.DataSeeding;
 using PokemonRenting.Repositories.Implementation;
 using PokemonRenting.Repositories.Infrastructure;
 using PokemonRenting.Web.CustomMiddleWare;
+using PokemonRenting.Web.Mapper;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<PokemonContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<PokemonContext>();
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
-builder.Services.AddAutoMapper(typeof(PokemonRepository));
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace PokemonRenting.Web
 {
-    app.UseMigrationsEndPoint();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<PokemonContext>(options =>
+                    options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddDefaultTokenProviders()
+                //.AddRoles<IdentityRole<Guid>>()
+                .AddEntityFrameworkStores<PokemonContext>();
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
+            builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
+
+
+
+            var config = new AutoMapper.MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new PokemonProfile(builder.Environment));
+            });
+            var mapper = config.CreateMapper();
+            builder.Services.AddSingleton(mapper);
+            builder.Services.AddSession(options =>
+            {
+                options.Cookie.Name = "PokemonProjectCookie";
+                options.IdleTimeout = TimeSpan.FromMinutes(1);
+                options.Cookie.HttpOnly = true;
+            });
+
+            builder.Services.AddRazorPages();
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = $"/Identity/Account/Login";
+            });
+
+        
+            var app = builder.Build();
+
+
+
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseMiddleware<ExceptionHandlerMiddleware>();
+         
+            app.UseStaticFiles();
+            DataSeeding();
+            app.UseSession();
+            app.UseRouting();
+          
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name:"areas",
+                     pattern: "{area:exists}/{controller=Pokemon}/{action=Index}/{id?}"
+                 );
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.MapRazorPages();
+
+            app.Run();
+            void DataSeeding()
+            {
+                using (var scope = app.Services.CreateScope())
+                {
+                    var DbInitial = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+                    DbInitial.Initialize();
+                }
+            }
+        }
+
+       
+    }
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-app.UseMiddleware<ExceptionHandlerMiddleware>();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Pokemon}/{action=Index}/{id?}");
-app.MapRazorPages();
-
-app.Run();
